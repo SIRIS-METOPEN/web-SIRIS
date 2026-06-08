bef960b9105c0dd42e9a3fa4209300b6941ddc23
Torikh <torikhnaser42@gmail.com>
2026-06-08 21:24:30 +0700

    chore: formatting

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -4,7 +4,10 @@ import { anonymousClient } from 'better-auth/client/plugins';
 export const authClient = createAuthClient({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000', // Ganti dengan URL backend jika berbeda
   fetchOptions: {
-    customFetchImpl: async (url: string | Request | URL, init?: RequestInit) => {
+    customFetchImpl: async (
+      url: string | Request | URL,
+      init?: RequestInit
+    ) => {
       return fetch(url, { ...init, credentials: 'include' });
     },
   },
